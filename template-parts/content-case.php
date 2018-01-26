1595ceb8cc9b752c8213a76aa602a1715ece3b99
marbaque <mario.badilla@gmail.com>
2018-01-25 21:41:34 -0600

    Update content-case.php

--- template-parts/content-case.php
+++ template-parts/content-case.php
@@ -7,7 +7,7 @@
  * @package flexieduca
  */
 ?>
-<a href="/mercadeo/casos-estrategias-comercio-movil/" title="<?php echo __( 'Back to gallery', 'flexieduca' ); ?>" class="back"><?php echo __( 'Back to gallery', 'flexieduca' ); ?></a>
+<a href="/?page_id=1809" title="<?php echo __( 'Back to gallery', 'flexieduca' ); ?>" class="back"><?php echo __( 'Back to gallery', 'flexieduca' ); ?></a>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
