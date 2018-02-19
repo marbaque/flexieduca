47542cd0867418a387d923b52d1c6ef58e172b01
Mario Badilla <mario.badilla@gmail.com>
2018-02-19 07:50:31 -0600

    pem-bup

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -130,9 +130,3 @@ function my_mce_before_init_insert_formats( $init_array ) {
 add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' ); 
 
 
-add_filter( 'rest_post_collection_params', 'my_prefix_add_rest_orderby_params', 10, 1 );
-function my_prefix_add_rest_orderby_params( $params ) {
-    $params['orderby']['enum'][] = 'menu_order';
-
-    return $params;
-}
