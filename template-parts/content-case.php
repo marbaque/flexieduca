74e8051ff65832879429cf8f79f3aa3b2b26b0c5
Mario Badilla <mario.badilla@gmail.com>
2018-05-02 14:04:26 -0600

    caso con feat img en vez de galería

--- template-parts/content-case.php
+++ template-parts/content-case.php
@@ -27,74 +27,30 @@
     <div class="entry-content case-content">
 
 	<div class="case__wrap">
-	    <?php
-	    // variables for custom filelds
-	    $img1	 = get_field('gallery_image_1');
-	    $img2	 = get_field('gallery_image_2');
-	    $img3	 = get_field('gallery_image_3');
-	    $img4	 = get_field('gallery_image_4');
-	    $img5	 = get_field('gallery_image_5');
-
-	    $sizeThumb	 = 'flexieduca-thumb';
-	    $sizeLg		 = 'flexieduca-gallery-img';
-
-	    $url = get_field('enlace');
-	    ?>
-	    <!--only image 1 is required-->
-	    <?php if (!empty($img1)): ?>
 
     	    <div class="gallery">
-    		<div id="photo-viewer"></div>
 
-    		<div id="thumbnails">
-    		    <a href="<?php echo wp_get_attachment_url($img1); ?>" class="thumb active" title="Imagen para <?php the_title(); ?>">
-			    <?php echo wp_get_attachment_image($img1, $sizeThumb); ?>
-    		    </a>
-
-
-			<?php if (!empty($img2)): ?>
-			    <a href="<?php echo wp_get_attachment_url($img2); ?>" class="thumb" title="Imagen para <?php the_title(); ?>">
-				<?php echo wp_get_attachment_image($img2, $sizeThumb); ?>
-			    </a>
-			<?php endif; ?>
-
-			<?php if (!empty($img3)): ?>
-			    <a href="<?php echo wp_get_attachment_url($img3); ?>" class="thumb" title="Imagen para <?php the_title(); ?>">
-				<?php echo wp_get_attachment_image($img3, $sizeThumb); ?>
-			    </a>
-			<?php endif; ?>
-
-			<?php if (!empty($img4)): ?>
-			    <a href="<?php echo wp_get_attachment_url($img4); ?>" class="thumb" title="Imagen para <?php the_title(); ?>">
-				<?php echo wp_get_attachment_image($img4, $sizeThumb); ?>
-			    </a>
-			<?php endif; ?>
-
-			<?php if (!empty($img5)): ?>
-			    <a href="<?php echo wp_get_attachment_url($img5) ?>" class="thumb" title="Imagen para <?php the_title(); ?>">
-				<?php echo wp_get_attachment_image($img5, $sizeThumb); ?>
-			    </a>
-			<?php endif; ?>
-    		</div>
-    		<div class="enlace">
+		    <?php if ( has_post_thumbnail() ) {
+			the_post_thumbnail();
+		    } ?>
+		
+		<?php $url = get_field('enlace'); ?>    
+		<div class="enlace">
 			<?php if (!empty($url)): ?>
 			    <a href="<?php echo $url; ?>" title="Enlace a <?php the_title(); ?>" target="_blank">
 				<?php echo preg_replace('#^https?://#', '', $url); ?></i></a>
 			    <?php endif; ?>
-    		</div>
-		
-    	    </div><!-- .case-gallery --> 
-	    
-	    
-
-	    <?php endif; ?>
-
-	    <div class="case-desc">
-		
+		</div>
 		<div class="case-trends">
 			<?php the_terms($post->ID, 'tendencia', 'Estrategia utilizada: ', ' ', ' '); ?>
     		</div>
 
+		
+    	    </div><!-- .case-gallery --> 
+	    
+	    <div class="case-desc">
+		
+		
 
 		<?php get_template_part('template-parts/auxiliar'); ?>
 
