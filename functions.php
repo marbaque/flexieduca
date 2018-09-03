7b7cb39b6cd2238ea3acd97a921d4a2a7de2e5fd
Mario Badilla <mario.badilla@gmail.com>
2018-09-03 13:07:10 -0600

    merge setiembre

--- functions.php
+++ functions.php
@@ -220,6 +220,15 @@ function flexieduca_widgets_init() {
         'before_title' => '<h2 class="widget-title">',
         'after_title' => '</h2>',
     ));
+	register_sidebar(array(
+        'name' => esc_html__('Barra lateral del menú principal', 'flexieduca'),
+        'id' => 'sidebar-contenidos',
+        'description' => esc_html__('Agregar widgets a la barra lateral de contenidos.', 'flexieduca'),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget' => '</section>',
+        'before_title' => '<h2 class="widget-title">',
+        'after_title' => '</h2>',
+    ));
 }
 
 add_action('widgets_init', 'flexieduca_widgets_init');
