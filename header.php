0fd28ce6768fd4d8366973b57682bff7a1a7b1a0
marbaque <mario.badilla@gmail.com>
2018-09-08 09:50:25 -0600

    boton de home si contenidos no tiene modilo asignado

--- header.php
+++ header.php
@@ -33,85 +33,89 @@
 					<?php if (is_singular( 'multimedia' )): //check if it is multimedia ?>
                     
 					
-                    <?php
-					// submenú de contenidos 
-						$current = $post->ID;
-						global $post_id;
-						$myvals = get_post_meta($post_id);
-						
-						if (is_array($myvals) || is_object($myvals))
-						{
-							foreach($myvals as $key=>$val)
+	                    <?php
+						// submenú de contenidos 
+							$current = $post->ID;
+							global $post_id;
+							$myvals = get_post_meta($post_id);
+							
+							if (is_array($myvals) || is_object($myvals))
 							{
-								echo $key . ' : ' . $val[0] . '<br/>';
+								foreach($myvals as $key=>$val)
+								{
+									echo $key . ' : ' . $val[0] . '<br/>';
+								}
 							}
-						}
-						
-						
-						$post_object = get_field('modulo_asignado');
-
-						if( $post_object ): 
-
-							// override $post
-							$post = $post_object;
-							setup_postdata( $post );
-							$myID = get_the_ID( $post );
-							$parent = get_the_title($post->ID);
-							$num = get_field('numero_modulo');
-							$color = get_field('color_modulo');
-							wp_reset_postdata();
-							//echo $myID; //test
-							
-							?>
-							<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false" type="button"><span><?php echo esc_html_e('Módulo ', 'flexieduca') . $num; ?></span></button>
-							<?php
 							
 							
-						
-
-							// The Query
-							$args = array(
-								'post_type' => 'multimedia',
-								'posts_per_page' => -1,
-								'orderby' => 'menu_order',
-								'order' => 'ASC',
-								'meta_query' => array(
-								array(
-									'key' => 'modulo_asignado',
-									'value' => $myID,
-									'compare' => 'LIKE'
-									)
-								)
-							);
-							$the_query = new WP_Query( $args );
-							// The Loop
-							if ( $the_query->have_posts() ) :
-								echo '<div class="menu-menu-principal-container">';
-								echo '<ul id="primary-menu" class="menu">';
-								echo '<li><a class="home" title="' . __('Ir al inicio', 'flexieduca') .'" href="' . site_url() . '" title="Inicio del curso">' . __('Inicio', 'flexieudca') .'</a></li>';
-								echo '<li><a class="modulo-item"  style="background-color:' . $color . ';color:white;" href="' . get_permalink($myID) . '" title="Inicio del curso">' . __('Módulo ', 'flexieduca') . $num . '. ' . $parent . '</a></li>';
-								while ( $the_query->have_posts() ) : $the_query->the_post();
-									// Do Stuff
-									$queried_object = get_the_ID();
-
-									if( $queried_object == $current) {
-										echo '<li class="current-item"><a href="' . get_permalink() . '" rel="bookmark">' . get_the_title() . '</a></li>';
-									} else {
-										echo '<li><a href="' . get_permalink() . '" rel="bookmark">' . get_the_title() . '</a></li>';
-									}
-
-								endwhile;
-								echo '</ul>';
-								echo '</div>';
-								// Reset Post Data
+							$post_object = get_field('modulo_asignado');
+	
+							if( $post_object ): 
+	
+								// override $post
+								$post = $post_object;
+								setup_postdata( $post );
+								$myID = get_the_ID( $post );
+								$parent = get_the_title($post->ID);
+								$num = get_field('numero_modulo');
+								$color = get_field('color_modulo');
 								wp_reset_postdata();
-							endif;
-							// Reset Post Data
-							wp_reset_postdata();
+								//echo $myID; //test
+								
+								?>
+								<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false" type="button"><span><?php echo esc_html_e('Módulo ', 'flexieduca') . $num; ?></span></button>
+								<?php
+								
+								
 							
-						endif; ?>		
+	
+								// The Query
+								$args = array(
+									'post_type' => 'multimedia',
+									'posts_per_page' => -1,
+									'orderby' => 'menu_order',
+									'order' => 'ASC',
+									'meta_query' => array(
+									array(
+										'key' => 'modulo_asignado',
+										'value' => $myID,
+										'compare' => 'LIKE'
+										)
+									)
+								);
+								$the_query = new WP_Query( $args );
+								// The Loop
+								if ( $the_query->have_posts() ) :
+									echo '<div class="menu-menu-principal-container">';
+									echo '<ul id="primary-menu" class="menu">';
+									echo '<li><a class="home" title="' . __('Ir al inicio', 'flexieduca') .'" href="' . site_url() . '" title="Inicio del curso">' . __('Inicio', 'flexieudca') .'</a></li>';
+									echo '<li><a class="modulo-item"  style="background-color:' . $color . ';color:white;" href="' . get_permalink($myID) . '" title="Inicio del curso">' . __('Módulo ', 'flexieduca') . $num . '. ' . $parent . '</a></li>';
+									while ( $the_query->have_posts() ) : $the_query->the_post();
+										// Do Stuff
+										$queried_object = get_the_ID();
+	
+										if( $queried_object == $current) {
+											echo '<li class="current-item"><a href="' . get_permalink() . '" rel="bookmark">' . get_the_title() . '</a></li>';
+										} else {
+											echo '<li><a href="' . get_permalink() . '" rel="bookmark">' . get_the_title() . '</a></li>';
+										}
+	
+									endwhile;
+									echo '</ul>';
+									echo '</div>';
+									// Reset Post Data
+									wp_reset_postdata();
+								endif;
+								// Reset Post Data
+								wp_reset_postdata(); ?>
+								
+						<?php else: ?>
+						
+							<a class="home-button" href="<?php echo home_url(); ?>"><span><?php esc_html_e('Inicio', 'flexieduca'); ?></span></a>
+							
+						<?php endif; ?>		
 					
-						<?php elseif( !is_singular('multimedia') && !is_front_page() ): ?>
+					<?php elseif( !is_singular('multimedia') && !is_front_page() ): ?>
 
 					<a class="home-button" href="<?php echo home_url(); ?>"><span><?php esc_html_e('Inicio', 'flexieduca'); ?></span></a>
 					
